dd43578f3712dea3dff30cb2f5c4ee378cc6c86d
Nanda <nanda.sankaran@gmail.com>
2022-05-18 09:41:37 +1000

    Export more styled components.

--- src/Views.ts
+++ src/Views.ts
@@ -1,2 +1,13 @@
-export { default as Box } from "terriajs/lib/Styled/Box";
-export { default as Icon } from "terriajs/lib/Styled/Icon";
+export { default as WorkflowPanel } from "terriajs/lib/ReactViews/Workflow/WorkflowPanel";
+export { default as AnimatedSpinnerIcon } from "terriajs/lib/Styled/AnimatedSpinnerIcon";
+export { default as Box, IBoxProps } from "terriajs/lib/Styled/Box";
+export { default as Button } from "terriajs/lib/Styled/Button";
+export { default as Checkbox } from "terriajs/lib/Styled/Checkbox/Checkbox";
+export { default as Icon, IconGlyph } from "terriajs/lib/Styled/Icon";
+export { default as Input, StyledTextArea } from "terriajs/lib/Styled/Input";
+export { default as Select } from "terriajs/lib/Styled/Select";
+export {
+  default as Text,
+  ITextProps,
+  TextSpan
+} from "terriajs/lib/Styled/Text";
