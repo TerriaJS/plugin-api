6afa3405d345f6c27f0645158842939ee6499786
Nanda <nanda.sankaran@gmail.com>
2022-12-19 15:40:31 +1100

    Update dist.

--- dist/TerriaPlugin.js
+++ dist/TerriaPlugin.js
@@ -1,8 +1,13 @@
-export function createPluginContext(viewState) {
+/**
+ * Creates a TerriaPluginContext with top-level objects accessible to the plugin.
+ *
+ */
+export function createPluginContext(viewState, pluginConfig) {
     return {
         viewState,
         terria: viewState.terria,
         workbench: viewState.terria.workbench,
-        overlays: viewState.terria.overlays
+        overlays: viewState.terria.overlays,
+        pluginConfig
     };
 }
