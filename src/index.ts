2b19abc6265b322912bad7d963a2499bdab1efc5
Nanda <nanda.sankaran@gmail.com>
2023-02-27 13:37:03 +1100

    Publish 0.0.1-alpha.8.
    Export:
      - UrlToCatalogMapping
      - loadBlob

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from "./TerriaPlugin";
 export * from "./Traits";
 export * from "./Views";
 export * from "./Hooks";
+export * from "./Helpers";
 
 import * as deprecated from "./Deprecated";
 export { deprecated };
